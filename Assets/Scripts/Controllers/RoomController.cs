f68327b073ec2b5ec1b4793a0d5765ded9c08453
Vildanix <Z1apvbslqa4mDz1DUO6G>
2016-12-10 17:33:06 +0100

    Separate grid and highlight to save triangle count. Unify instantiating grid in room

--- Assets/Scripts/Controllers/RoomController.cs
+++ Assets/Scripts/Controllers/RoomController.cs
@@ -2,27 +2,52 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(MeshRenderer))]
-[RequireComponent(typeof(MeshFilter))]
 public class RoomController : MonoBehaviour {
     enum ROOM_SIDES {FLOOR, WALL_N, WALL_E, WALL_S, WALL_W, CEILING};
     Dictionary<ROOM_SIDES, RoomGrid> roomSides;
-    MeshRenderer meshRenderer;
-    MeshFilter meshFilter;
+    RoomGrid highlight;
+
+    // presets
+    public GameObject gridPrefab;
+    public Material highlightMat;
+    public Material gridBaseMat;
+
+    public int gridWidth = 20;
+    public int gridHeight = 20;
+    public float cubeSize = 5.0f;
 
     void Awake () {
-        meshRenderer = GetComponent<MeshRenderer>();
-        meshFilter = GetComponent<MeshFilter>();
+        if (!gridPrefab) {
+            Debug.LogError("RoomController missing grid prefab");
+        }
+
+        highlight = InstantiateGridSide(new Vector3(-cubeSize / 2, 0.01f, -cubeSize / 2), new Vector3(0, 0, 0), 0.15f, highlightMat);
 
         // initialize room
         roomSides = new Dictionary<ROOM_SIDES, RoomGrid>();
 
-        RoomGrid currentGrid = new RoomGrid();
-        currentGrid.CreateGrid(20, 20);
-        meshFilter.mesh = currentGrid.getGridMesh();
+        // floor
+        RoomGrid currentGrid = InstantiateGridSide(new Vector3(-cubeSize / 2, 0f, -cubeSize / 2), new Vector3(0, 0, 0), 0.0f, gridBaseMat);
         roomSides.Add(ROOM_SIDES.FLOOR, currentGrid);
-        
-	}
+
+        currentGrid = InstantiateGridSide(new Vector3(-cubeSize / 2, 0f, -cubeSize / 2), new Vector3(0, 0, 0), 0.0f, gridBaseMat);
+        roomSides.Add(ROOM_SIDES.FLOOR, currentGrid);
+
+    }
+
+    // instantiate grid on given position and return RoomGrid component on new grid
+    private RoomGrid InstantiateGridSide(Vector3 position, Vector3 rotation, float spacing, Material gridMat) {
+        GameObject gridObj = Instantiate(gridPrefab);
+        gridObj.transform.localScale = new Vector3(cubeSize / gridWidth, 1, cubeSize / gridHeight);
+        gridObj.transform.localPosition = position;
+        gridObj.transform.Rotate(rotation);
+        gridObj.GetComponent<MeshRenderer>().material = gridMat;
+
+        RoomGrid grid = gridObj.GetComponent<RoomGrid>();
+        grid.CreateGrid(gridWidth, gridHeight, spacing);
+
+        return grid;
+    }
 	
 	// Update is called once per frame
 	void Update () {
--- Assets/Scripts/RoomGrid.cs
+++ Assets/Scripts/RoomGrid.cs
@@ -2,53 +2,60 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class RoomGrid {
+[RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(MeshFilter))]
+[ExecuteInEditMode]
+public class RoomGrid : MonoBehaviour{
     private Mesh gridMesh;
-    private Mesh highlightMesh;
 
     // building structures
-    private List<Vector3> gridVertices = new List<Vector3>();
-    private List<Vector3> highlightVertices = new List<Vector3>();
+    private List<Vector3> gridVertices;
+    private List<int> gridTriangles;
+    private List<Vector2> gridUVs;
+    private List<Vector4> gridTangets;
 
-    private List<int> gridTriangles = new List<int>();
-    private List<int> highlightTriangles = new List<int>();
+    MeshFilter meshFilter;
 
-    private List<Vector2> gridUVs = new List<Vector2>();
-    private List<Vector2> hightlightUVs = new List<Vector2>();
+    public void Awake() {
+        gridVertices = new List<Vector3>();
+        gridTriangles = new List<int>();
+        gridUVs = new List<Vector2>();
+        gridTangets = new List<Vector4>();
+        
+        meshFilter = GetComponent<MeshFilter>();
+    }
 
     // create new meshes for grid. (Max 150 x 100 grid)
-    public void CreateGrid(int cell_x, int cell_y) {
-
+    public void CreateGrid(int cell_x, int cell_y, float spacing = 0.0f) {
         gridMesh = new Mesh();
-        gridMesh.name = "Grid Plane";
-
-        highlightMesh = new Mesh();
-        highlightMesh.name = "Highlight Cells";
+        gridMesh.name = "Room Grid plane";
 
         // create mesh vertices. Each grid cell have separate quad
         for (int i = 0, y = 0; y <= cell_y; y++) {
             for (int x = 0; x <= cell_x; x++, i++) {
-                CreateGridQuad(i, x, y, new Vector2(0, 0));
+                CreateGridQuad(i, x, y, new Vector2(0, 0), spacing);
             }
         }
 
-
         gridMesh.vertices = gridVertices.ToArray();
-        highlightMesh.vertices = highlightVertices.ToArray();
 
         gridMesh.triangles = gridTriangles.ToArray();
-        highlightMesh.triangles = highlightTriangles.ToArray();
 
         gridMesh.uv = gridUVs.ToArray();
-        highlightMesh.uv = hightlightUVs.ToArray();
+
+        gridMesh.RecalculateNormals();
+
+        gridMesh.tangents = gridTangets.ToArray();
+
+        meshFilter.mesh = gridMesh;
     }
 
-    private void CreateGridQuad(int cellIndex, int x, int y, Vector2 textureOffset) {
+    private void CreateGridQuad(int cellIndex, int x, int y, Vector2 textureOffset, float spacing) {
         // new vertices for quad
-        gridVertices.Add(new Vector3(x, 0, y));
-        gridVertices.Add(new Vector3(x + 1, 0, y));
-        gridVertices.Add(new Vector3(x + 1, 0, y + 1));
-        gridVertices.Add(new Vector3(x, 0, y + 1));
+        gridVertices.Add(new Vector3(x + spacing, 0, y + spacing));
+        gridVertices.Add(new Vector3(x - spacing + 1, 0, y + spacing));
+        gridVertices.Add(new Vector3(x - spacing + 1, 0, y - spacing + 1));
+        gridVertices.Add(new Vector3(x + spacing, 0, y - spacing + 1));
 
         // two triangles for new vertex quad
         gridTriangles.Add(cellIndex * 4);
@@ -63,16 +70,16 @@ public class RoomGrid {
         gridUVs.Add(new Vector2(textureOffset.x + 0.5f, textureOffset.y));
         gridUVs.Add(new Vector2(textureOffset.x + 0.5f, textureOffset.y + 0.5f));
         gridUVs.Add(new Vector2(textureOffset.x, textureOffset.y + 0.5f));
+
+        gridTangets.Add(new Vector4(1f, 0f, 0f, -1f));
+        gridTangets.Add(new Vector4(1f, 0f, 0f, -1f));
+        gridTangets.Add(new Vector4(1f, 0f, 0f, -1f));
+        gridTangets.Add(new Vector4(1f, 0f, 0f, -1f));
     }
 
     // get generated mesh for grid
     public Mesh getGridMesh() {
         return gridMesh;
-    }
-
-    // get generated mesh for highlight
-    public Mesh getHighlightMesh() {
-        return highlightMesh;
     }
     
 }
